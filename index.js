1b476de83634be8ee42a8e4ae3e98bb0eb38d37e
S-H-A-K-H-Z-O-D <M.Shakhzod2002@gmail.com>
2023-03-10 17:35:00 +0500

    starting point of learning javaScript

---
+++ index.js
@@ -0,0 +1,34 @@
+alert("Welcome to our wep pag! We have some questions to know who you are.");
+
+var userName = prompt("Input your name, please!");
+
+var surname = prompt("Input your surname, please!");
+
+var job = prompt("Input your job, please!");
+
+var age = prompt("Input your age, please!");
+
+var gender = confirm("Are you male?");
+
+var family = prompt("May you give basic information about your family?");
+
+var reason = prompt("Why are you here?");
+
+var javascript = prompt("Do you know what is JavaScript?");
+
+var language = prompt("Do you know other languages too?");
+
+var mood = prompt("Are you happy with us?");
+
+console.log(userName);
+console.log(surname);
+console.log(job);
+console.log(age);
+console.log(gender);
+console.log(family);
+console.log(reason);
+console.log(javascript);
+console.log(language);
+console.log(mood);
+
+alert("Thank you for your answers!")
